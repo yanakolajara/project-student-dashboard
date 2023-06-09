31e2bf35eaf965d1cd2c551bb528e5665643d669
Yanako Lajara <yanakolajara@pursuit.org>
2023-06-09 05:35:08 -0400

    student list structure created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import students from './data/data.json'
 
 function App() {
   return (
     <div>
       <h1>Student Dashboard</h1>
+      <div class="students">
+        <h2>All students</h2>
+        <p class="totalStudents" value={() => {}}>Total students: {}</p> 
+        <div class="studentCard">
+          <img class="studentImg"></img>
+          <div class="studentInfo">
+            <p class="studentName"></p>
+            <p class="studentDOB"></p>
+            <p class="studentEmail"></p>
+            <button>Show more...</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
