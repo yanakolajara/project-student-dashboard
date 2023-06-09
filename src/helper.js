49f76a7227539b1496a4cc9273bef90aa4b8c381
Yanako Lajara <yanakolajara@pursuit.org>
2023-06-09 06:26:49 -0400

    mockup

---
+++ src/helper.js
@@ -0,0 +1,11 @@
+import data from './data/data.json'
+
+//* From spring 2025 to winter 2026
+export const cohorts = () => {
+
+}
+
+//todo: This functions adds all students of the parameter's cohort
+export const cohortStudents = (cohort) => {
+    
+}
